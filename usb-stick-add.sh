7a0df2fc06df43b514196f8cb571b7eb26f8118c
Robert Forsman <git@thoth.purplefrog.com>
2019-03-23 16:09:19 -0400

    if there isn't a node.lua, assume there's a looop.mp4 for omxplayer

--- usb-stick-add.sh
+++ usb-stick-add.sh
@@ -7,8 +7,12 @@ if [ -d $DIR ]; then
 
     skill loop-video.sh
     skill omxplayer
+    if [ -f $DIR/node.lua ] ; then
 
-    INFOBEAMER_BLANK_MODE=layer $HOME/info-beamer-pi/info-beamer $DIR
+	INFOBEAMER_BLANK_MODE=layer $HOME/info-beamer-pi/info-beamer $DIR
+    else
+	omxplayer -b -o hdmi --loop --no-osd $DIR/loop.mp4
+    fi
 else
     echo "USB media has no ngaa/ subdirectory"
 fi
